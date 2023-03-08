22666e9bb28b918e639221f4cb6441284a5828ab
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2023-03-08 10:03:38 +0100

    Voorbeeld met ChatGPT.
    Voorbeeld met ChatGPT gemaakt.

--- sx1509_voorbeeld.py
+++ sx1509_voorbeeld.py
@@ -1,23 +1,41 @@
-from machine import I2C, Pin
-import time
-from sx1509 import SX1509_Keypad
-from sx1509 import SX1509_LEDDriver
+import utime
+import random
+from machine import Pin, PWM, ADC
+import esp
+import gc
+esp.osdebug(None)
 
-# Initialize I2C interface and SX1509 IC address
-i2c = I2C(scl=Pin(22), sda=Pin(21))
-sx1509_address = 0x3E
+gc.collect()
 
-# Initialize the SX1509 LED driver and the LED it controls
-led_driver = SX1509_LEDDriver(i2c, sx1509_address, 1)
-led = led_driver.leds[0]
+class mainAPP:
+    def __init__(self):
+        self._adc_pin = 13
+        self._adc = (Pin(self._adc_pin))
+        random.seed(1024)        
 
-# Initialize the SX1509 keypad
-keypad = SX1509_Keypad(i2c, sx1509_address)
-
-while True:
-    key = keypad.scan() # Scan for keypad input
-    
-    if key == 'A':
-        # Breathe the LED if the 'A' key is pressed
-        led.breathe(1, 255, 0.5, 0.5)
-
+    def main(self):
+        aantalLoops =  1000/60 * 60 * 60 * 24
+        
+        
+        led = PWM(Pin(self._adc_pin))
+        led.freq(500)
+        
+        for i1 in range(0, aantalLoops):
+            duty1 = int(random.random()*1000)
+            for dutCnt1 in range(1, int(random.random()*100)):
+                led.duty(duty1)
+                utime.sleep_ms(int(random.random()*100))
+            
+            led.duty(0)
+            sleepTime1 = int(random.random()*1000)
+            print(f'sleepTime1: {sleepTime1}ms')
+            utime.sleep_ms(sleepTime1)
+            if(i1 % 10 == 0):
+                print("Led knipperen op PIN=",self._adc_pin ,  i1,"van",aantalLoops)
+   
+   
+if __name__ == '__main__':
+    print("App start")
+    main1 = mainAPP()
+    main1.main()
+    print("App eind")
